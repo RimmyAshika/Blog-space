0d007c56bd7e8ebc1ef8bfd8372e9729898e21e5
RimmyAshika <160113752+RimmyAshika@users.noreply.github.com>
2025-09-16 15:39:15 +0530

    intial file added

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import appwriteService from '../appwrite/Config/';
-import { Container, PostForm } from '../components';
+import React, {useEffect, useState} from 'react'
+import {Container, PostForm} from '../components/Index.js'
+import appwriteService from "../appwrite/config.js";
+import { useNavigate,  useParams } from 'react-router-dom';
 
 function EditPost() {
-    const [post, setPost] = useState(null);
-    const { slug } = useParams();
-    const navigate = useNavigate();
+    const [post, setPosts] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (slug) {
             appwriteService.getPost(slug).then((post) => {
                 if (post) {
-                    setPost(post);
+                    setPosts(post)
                 }
-            });
+            })
         } else {
-            navigate('./');
+            navigate('/')
         }
-    }, [slug, navigate]);
-    return (
-        <div className="py-8 flex text-left">
-            <Container>
-                <PostForm post={post} />
-            </Container>
-        </div>
-    );
+    }, [slug, navigate])
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ) : null
 }
 
-export default EditPost;
+export default EditPost
